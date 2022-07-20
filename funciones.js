3ab157767acfecd97089cb897501beb129fbeb36
Ander <anderns2000@gmail.com>
2022-07-20 22:05:57 +0200

    Version 2 agenda GAS

--- funciones.js
+++ funciones.js
@@ -4,3 +4,9 @@ function doGet(){
 function obtenerDatosHtml(nombre){
     return HtmlService.createHtmlOutputFromFile(nombre).getContent();
 }
+
+function obtenerContactos(){
+    let hoja = SpreadsheetApp.openById('1mp6aOvS0HVdOaiTGJG8xqrKaJ4MjQ4v4CmQHNEO3BlI').getActiveSheet();
+    let datos = hoja.getDataRange().getValues();
+    return datos
+}
